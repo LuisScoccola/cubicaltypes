246419bc0c2a1dd1541b565e02543de0e8e2ebd4
Luis Scoccola <luis.scoccola@gmail.com>
2017-01-23 13:21:22 -0500

    A little more about commutativity of the product of two 1-CT. We have an equivalence between the 0-CTs and the 1-CTs, missing the 2-cells.

--- CubicalTypeProdExp.v
+++ CubicalTypeProdExp.v
@@ -3,12 +3,20 @@ Require Import CubicalType.
 Require Import CubicalTypeExamples.
 
 
+(* *)
+Local Unset Elimination Schemes.
+
 (* product of 1-semi-cubical types *)
 Definition prod_zerocells (C D : CT1) : Type := C.1 * D.1.
 
 Inductive prod_onecells (C D : CT1) : prod_zerocells C D -> prod_zerocells C D -> Type :=
   | edge_vert (i0 i1 : C.1) (ai : C.2 i0 i1) (j : D.1) : prod_onecells C D (i0, j) (i1, j)
   | vert_edge (i : C.1) (j0 j1 : D.1) (aj : D.2 j0 j1) : prod_onecells C D (i, j0) (i, j1).
+
+Scheme prod_onecells_ind := Induction for prod_onecells Sort Type.
+Scheme prod_onecells_rec := Minimality for prod_onecells Sort Type.
+
+Definition prod_onecells_rect := prod_onecells_ind.
 
 Arguments edge_vert {C D i0 i1} ai j.
 Arguments vert_edge {C D} i {j0 j1} aj.
@@ -28,29 +36,86 @@ Arguments square {C D} {i0 i1} ai {j0 j1} aj.
 
 Definition CT1_product (C D : CT1) : CT2 :=
   (prod_zerocells C D ; (prod_onecells C D ; prod_twocells C D) ).
-   
-(* product is commutative, unfinished
-Definition commute `{Univalence}
-                   (C D : CT1) : CT1_product C D = CT1_product D C.
+
+
+(* now we want to prove that the product is commutative *)
+Definition prod_zerocells_commute `{Univalence}
+                                  (C D : CT1) :
+                                    (CT1_product C D).1 = (CT1_product D C).1 :=
+  equiv_path_universe _ _ (equiv_prod_symm C.1 D.1).
+
+
+Definition flip {A B : Type} : A * B -> B * A :=
+  fun ab => (snd ab , fst ab).
+
+(* not needed *)
+Definition flip_involution {A B : Type} (ab : A * B) : flip (flip ab) = ab :=
+  match ab with
+    | (a , b) => reflexivity _
+  end.
+
+
+Definition prod_onecells_map_from {C D : CT1} (x y : prod_zerocells D C) :
+                                  (CT1_product D C).2.1 x y -> (CT1_product C D).2.1 (flip x) (flip y) :=
+  fun X => match X with
+             | edge_vert i0 i1 ai j => vert_edge j ai
+             | vert_edge i j0 j1 aj => edge_vert aj i
+           end.
+
+
+Definition prod_onecells_map_to {C D : CT1} (x y : prod_zerocells D C) :
+                                (CT1_product C D).2.1 (flip x) (flip y) -> (CT1_product D C).2.1 x y :=
+  let (x0 , x1) := x in
+  let (y0 , y1) := y in prod_onecells_map_from (x1 , x0) (y1 , y0).
+
+
+Definition prod_onecells_map_from_to {C D : CT1} (x y : prod_zerocells D C) :
+                                     (prod_onecells_map_to x y) o (prod_onecells_map_from x y) == idmap :=
+  fun X => match X with
+             | edge_vert _ _ _ _ => reflexivity _
+             | vert_edge _ _ _ _ => reflexivity _
+           end.
+
+
+Definition prod_onecells_map_to_from {C D : CT1} (x y : prod_zerocells D C) :
+                                     (prod_onecells_map_from x y) o (prod_onecells_map_to x y) == idmap :=
+  let (x0 , x1) := x in
+  let (y0 , y1) := y in prod_onecells_map_from_to (x1 , x0) (y1 , y0).
+
+
+Definition prod_onecells_commute `{Univalence}
+             (C D : CT1) :
+             (prod_zerocells_commute C D) # (CT1_product C D).2.1 = (CT1_product D C).2.1.
 Proof.
   pose (flip_equiv := equiv_prod_symm C.1 D.1).
-  pose (flip_equal := equiv_path_universe _ _ flip_equiv).
-  simple refine (path_sigma _ _ _ _ _).
-    - exact flip_equal.
-    - simple refine (path_sigma _ _ _ _ _).
-      + rewrite (transport_sigma _ _). simpl.
-        simple refine (path_forall _ _ _). intro x.
-        simple refine (path_forall _ _ _). intro y.
-        refine (inverse _).
-        simple refine (path_universe_uncurried _).
-        simple refine (BuildEquiv _ _ _ _).
-          * intro. induction X.
-              -- rewrite (transport_arrow _ _ _).
-                 rewrite (transport_arrow _ _ _). 
-                 rewrite (transport_path_universe_V_uncurried flip_equiv _).
-                 rewrite (transport_path_universe_V_uncurried flip_equiv _).
-*)
+  (* we prove it point-wise *)
+  simple refine (path_forall _ _ _). intro x.
+  simple refine (path_forall _ _ _). intro y.
+  simple refine (path_universe_uncurried _).
+  (* todo: don't use rewrite *)
+  rewrite (transport_arrow _ _ _).
+  rewrite (transport_arrow _ _ _). 
+  rewrite transport_const.
+  rewrite (transport_path_universe_V_uncurried flip_equiv _).
+  rewrite (transport_path_universe_V_uncurried flip_equiv _).
+  simple refine (BuildEquiv _ _ _ _).
+    - exact (prod_onecells_map_to x y).
+    - simple refine (isequiv_biinv _ _).
+      exact ((prod_onecells_map_from x y ; prod_onecells_map_to_from x y) ,
+             (prod_onecells_map_from x y ; prod_onecells_map_from_to x y) ).
+Qed.
 
+
+(* product is commutative, unfinished
+
+Definition prod_twocells_commute `{Univalence}
+             (C D : CT1) :
+             (prod_onecells_commute C D) # (CT1_product C D).2.2 = (CT1_product D C).2.2.
+
+
+Definition commute `{Univalence}
+                   (C D : CT1) : CT1_product C D = CT1_product D C.
+*)
 
 (* 1-natural transformation: if [C] is a 1-CT and [H] is a 2-CT
    we form the 1-CT [H^C].
