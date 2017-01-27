970c991d3e232fbcc0f058177ef3797f30b61531
Luis Scoccola <luis.scoccola@gmail.com>
2017-01-27 14:20:48 -0500

    Defined composition of (1,2,3) CT morphisms.

--- CubicalType.v
+++ CubicalType.v
@@ -57,6 +57,7 @@ Definition CT3toCT2 (C : CT3) : CT2 :=
 Coercion CT3toCT2 : CT3 >-> CT2.
 
 
+(* morphisms *)
 
 Definition combinatorial_arrows_morph (C D : CT1) (M0 : C.1 -> D.1) : Type :=
   forall c0 c1 : C.1, C.2 c0 c1 -> D.2 (M0 c0) (M0 c1).
@@ -105,3 +106,49 @@ Definition CT3_morphism (C D : CT3) : Type :=
  { M2 : combinatorial_squares_morph C D M1 &
         combinatorial_cubes_morph C D M2
  }}}.
+
+Coercion CT2mtoCT1m {C D : CT2} (m : CT2_morphism C D) : CT1_morphism C D :=
+  (m.1 ; m.2.1).
+
+Coercion CT3mtoCT2m {C D : CT3} (m : CT3_morphism C D) : CT2_morphism C D :=
+  (m.1 ; (m.2.1 ; m.2.2.1)).
+
+
+
+
+(* composition of morphisms *)
+
+Definition CT1_morphism_comp {C D E : CT1}
+             (F : CT1_morphism C D) (G : CT1_morphism D E) : CT1_morphism C E.
+Proof.
+  exists (G.1 o F.1).
+  intro c0. intro c1.
+  exact ( (G.2 (F.1 c0) (F.1 c1)) o (F.2 _ _) ).
+Defined.
+(* why doesn't this work?
+  exact ( (G.1 o F.1 ; fun c0 c1 => (G.2 (F.1 c0) (F.1 c1)) o (F.2 _ _)) ).
+*)
+
+Definition CT2_morphism_comp {C D E : CT2}
+             (F : CT2_morphism C D) (G : CT2_morphism D E) : CT2_morphism C E.
+Proof.
+  exists ((CT1_morphism_comp F G).1).
+  exists ((CT1_morphism_comp F G).2).
+  intros v00 v01 v10 v11 ax0 ax1 a0x a1x c.
+  exact (G.2.2 _ _ _ _ _ _ _ _ (F.2.2 _ _ _ _ _ _ _ _ c)).
+Defined.
+
+Definition CT3_morphism_comp {C D E : CT3}
+             (F : CT3_morphism C D) (G : CT3_morphism D E) : CT3_morphism C E.
+Proof.
+  exists ((CT2_morphism_comp F G).1).
+  exists ((CT2_morphism_comp F G).2.1).
+  exists ((CT2_morphism_comp F G).2.2).
+  intros v000 v001 v010 v011 v100 v101 v110 v111.
+  intros a00x a01x a10x a11x a0x0 a0x1 a1x0 a1x1 ax00 ax01 ax10 ax11.
+  intros f0xx f1xx fx0x fx1x fxx0 fxx1.
+  intro c.
+  exact (G.2.2.2 _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+          (F.2.2.2 _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ c)).
+Defined.
+
